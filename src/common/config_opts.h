5ffa888d360ad09b04ed7ccf74d33eefe7750959
Jiaying Ren <jiaying.ren@umcloud.com>
2017-04-09 15:58:38 +0800

    rgw: bl impl upload opslog
    Signed-off-by: Jiaying Ren <jiaying.ren@umcloud.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1436,6 +1436,7 @@ OPTION(rgw_enable_bl_threads, OPT_BOOL, true)
 OPTION(rgw_bl_max_objs, OPT_INT, 32)
 OPTION(rgw_bl_lock_max_time, OPT_INT, 60)  // total run time for a single bl processor work
 OPTION(rgw_bl_work_time, OPT_STR, "00:00-06:00") //job process bl at 00:00-06:00s
+OPTION(rgw_bl_url, OPT_STR, "http://localhost:7480") // modify according to the rgw_frontend config
 
 OPTION(rgw_data, OPT_STR, "/var/lib/ceph/radosgw/$cluster-$id")
 OPTION(rgw_enable_apis, OPT_STR, "s3, s3website, swift, swift_auth, admin")
