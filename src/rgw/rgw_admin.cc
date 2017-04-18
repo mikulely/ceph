9c4363329728c6962f95bae76236a62e806858fe
Jiaying Ren <jiaying.ren@umcloud.com>
2017-04-18 11:33:53 +0800

    rgw: bl fix rgw-admin log show filter by bucket
    Signed-off-by: Jiaying Ren <jiaying.ren@umcloud.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -4952,9 +4952,10 @@ int main(int argc, const char **argv)
     }
 
     if (!bucket_id.empty() && !bucket_name.empty()) {
-	filter = bucket_id;
-	filter += "-";
-	filter += bucket_name;
+      filter = bucket_id;
+      filter += "-";
+      filter += bucket_name;
+      filter_by_date = false;
     }
 
 
