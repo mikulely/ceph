4beee7471ffc839cc1ed5e550c8f82fd98cb8f44
Jiaying Ren <jiaying.ren@umcloud.com>
2017-06-27 01:55:57 +0800

    rgw: subuser usage log show
    Signed-off-by: Jiaying Ren <jiaying.ren@umcloud.com>

--- src/rgw/rgw_usage.cc
+++ src/rgw/rgw_usage.cc
@@ -8,8 +8,33 @@
 #include "rgw_usage.h"
 #include "rgw_formats.h"
 
+#define dout_subsys ceph_subsys_rgw
+
 using namespace std;
 
+static void dump_subuser_usage_categories_info(Formatter *formatter, const rgw_usage_log_entry& entry,
+                                               map<string, bool> *categories, const string subuser)
+{
+  formatter->open_array_section("categories");
+  auto subuser_iter = entry.subuser_usage_map.find(subuser);
+  if (subuser_iter != entry.subuser_usage_map.end()) {
+    for (auto category_iter : subuser_iter->second) {
+      if (categories && !categories->empty() && !categories->count(category_iter.first))
+        continue;
+
+      const rgw_usage_data& usage = category_iter.second;
+      formatter->open_object_section("entry");
+      formatter->dump_string("category", category_iter.first);
+      formatter->dump_int("bytes_sent", usage.bytes_sent);
+      formatter->dump_int("bytes_received", usage.bytes_received);
+      formatter->dump_int("ops", usage.ops);
+      formatter->dump_int("successful_ops", usage.successful_ops);
+      formatter->close_section(); // entry
+    }
+  }
+
+  formatter->close_section(); // categories
+}
 
 static void dump_usage_categories_info(Formatter *formatter, const rgw_usage_log_entry& entry, map<string, bool> *categories)
 {
@@ -30,14 +55,14 @@ static void dump_usage_categories_info(Formatter *formatter, const rgw_usage_log
   formatter->close_section(); // categories
 }
 
-int RGWUsage::show(RGWRados *store, rgw_user& uid, uint64_t start_epoch,
+int RGWUsage::show(RGWRados *store, rgw_user& uid, const string& subuser, uint64_t start_epoch,
 		   uint64_t end_epoch, bool show_log_entries, bool show_log_sum,
 		   map<string, bool> *categories,
 		   RGWFormatterFlusher& flusher)
 {
   uint32_t max_entries = 1000;
-
   bool is_truncated = true;
+  bool by_subuser = !subuser.empty();
 
   RGWUsageIter usage_iter;
   Formatter *formatter = flusher.get_formatter();
@@ -79,23 +104,45 @@ int RGWUsage::show(RGWRados *store, rgw_user& uid, uint64_t start_epoch,
           }
           formatter->open_object_section("user");
           formatter->dump_string("user", ub.user);
+	  if (by_subuser)
+              formatter->dump_string("subuser", subuser);
           formatter->open_array_section("buckets");
           user_section_open = true;
           last_owner = ub.user;
         }
-        formatter->open_object_section("bucket");
-        formatter->dump_string("bucket", ub.bucket);
-        utime_t ut(entry.epoch, 0);
-        ut.gmtime(formatter->dump_stream("time"));
-        formatter->dump_int("epoch", entry.epoch);
-        string owner = entry.owner.to_str();
-        string payer = entry.payer.to_str();
-        formatter->dump_string("owner", owner);
-        if (!payer.empty() && payer != owner) {
-          formatter->dump_string("payer", payer);
-        }
-        dump_usage_categories_info(formatter, entry, categories);
-        formatter->close_section(); // bucket
+	if (by_subuser){
+          auto subiter = entry.subuser_usage_map.find(subuser);
+          if (subiter != entry.subuser_usage_map.end()) {
+            formatter->open_object_section("bucket");
+            formatter->dump_string("bucket", ub.bucket);
+            utime_t ut(entry.epoch, 0);
+            ut.gmtime(formatter->dump_stream("time"));
+            formatter->dump_int("epoch", entry.epoch);
+            string owner = entry.owner.to_str();
+            string payer = entry.payer.to_str();
+            formatter->dump_string("owner", owner);
+            if (!payer.empty() && payer != owner) {
+              formatter->dump_string("payer", payer);
+            }
+            dump_subuser_usage_categories_info(formatter, entry, categories, subuser);
+            formatter->close_section(); // bucket
+	  }
+	} else {
+          formatter->open_object_section("bucket");
+          formatter->dump_string("bucket", ub.bucket);
+          utime_t ut(entry.epoch, 0);
+          ut.gmtime(formatter->dump_stream("time"));
+          formatter->dump_int("epoch", entry.epoch);
+          string owner = entry.owner.to_str();
+          string payer = entry.payer.to_str();
+          formatter->dump_string("owner", owner);
+          if (!payer.empty() && payer != owner) {
+            formatter->dump_string("payer", payer);
+          }
+          dump_usage_categories_info(formatter, entry, categories);
+          formatter->close_section(); // bucket
+	}
+
         flusher.flush();
       }
 
@@ -117,9 +164,17 @@ int RGWUsage::show(RGWRados *store, rgw_user& uid, uint64_t start_epoch,
       const rgw_usage_log_entry& entry = siter->second;
       formatter->open_object_section("user");
       formatter->dump_string("user", siter->first);
-      dump_usage_categories_info(formatter, entry, categories);
+      if (by_subuser) {
+        formatter->dump_string("subuser", subuser);
+        dump_subuser_usage_categories_info(formatter, entry, categories, subuser);
+      } else {
+        dump_usage_categories_info(formatter, entry, categories);
+      }
       rgw_usage_data total_usage;
-      entry.sum(total_usage, *categories);
+      if (by_subuser)
+        entry.sum(subuser, total_usage, *categories);
+      else 
+        entry.sum(total_usage, *categories);
       formatter->open_object_section("total");
       formatter->dump_int("bytes_sent", total_usage.bytes_sent);
       formatter->dump_int("bytes_received", total_usage.bytes_received);
